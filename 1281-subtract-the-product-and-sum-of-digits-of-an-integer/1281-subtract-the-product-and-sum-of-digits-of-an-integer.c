7d4f117b3d6100511211c9f310b58b83bc737913
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-05-20 11:00:29 +0800

    Time: 0 ms (100.00%), Space: 5.4 MB (65.62%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.c
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.c
@@ -1,12 +1,10 @@
 int subtractProductAndSum(int n){
-    int sum = 0, result = 0;
-    int product = 1;
-    while (n != 0)
+    int sum = 0, product = 1;
+    while (n > 0)
     {
         product *= (n % 10);
         sum += (n % 10);
         n = n / 10;
     }
-    result = product - sum;
-    return result;
+    return product-sum;
 }
