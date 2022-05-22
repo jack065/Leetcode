42432e435e706ed7f70ca560b6716ac5bbcac72b
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-05-22 11:13:23 +0800

    Time: 1 ms (95.66%), Space: 41.4 MB (43.94%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum = 0;
+        int counter = 0;
+        while (counter < 10){
+            while (n > 0){
+                int placeholder = n % 10;
+                sum += Math.pow(placeholder, 2);
+                n /= 10;
+            }
+            if (sum == 1){
+                return true;
+            }
+            n = sum;
+            sum = 0;
+            counter++;
+        }
+        return false;
+    }
+}
