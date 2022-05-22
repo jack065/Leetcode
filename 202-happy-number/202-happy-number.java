837219aa28e7e2ba65b6ec667deef113880b8f01
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-05-22 11:14:13 +0800

    Time: 1 ms (95.66%), Space: 42 MB (9.94%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isHappy(int n) {
         int sum = 0;
         int counter = 0;
-        while (counter < 10){
+        while (counter < 10){ //Sets a time out condition. Less optimal for larger numbers
             while (n > 0){
                 int placeholder = n % 10;
                 sum += Math.pow(placeholder, 2);
