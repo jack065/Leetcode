3d64c403d98898d89ff529b960aca3016930fb2d
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-05-27 22:40:31 +0800

    Time: 1 ms (88.10%), Space: 42.3 MB (53.09%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,18 +1,14 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int pointer1 = 0, pointer2 = 0, pointer = 0;
-        int length1 = word1.length(), length2 = word2.length();
-        char[] results = new char[length1 + length2];
-        while ((pointer1 < length1) || (pointer2 < length2)){
-            if (pointer1 < length1){
-                results[pointer] = word1.charAt(pointer1);
-                pointer++;
-                pointer1++;
+        int p1 = 0, p2 = 0, p = 0;
+        int l1 = word1.length(), l2 = word2.length();
+        char[] results = new char[l1 + l2];
+        while ((p1 < l1) || (p2 < l2)){
+            if (p1 < l1){
+                results[p++] = word1.charAt(p1++);
             }
-            if (pointer2 < length2){
-                results[pointer] = word2.charAt(pointer2);
-                pointer++;
-                pointer2++;
+            if (p2 < l2){
+                results[p++] = word2.charAt(p2++);
             }
         }
         return new String(results);
