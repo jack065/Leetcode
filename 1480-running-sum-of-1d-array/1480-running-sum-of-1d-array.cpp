90745a8f8fa81736acafadd1f428466cb8e610fa
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-01 23:49:25 +0800

    Time: 4 ms (59.62%), Space: 8.5 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,6 +1,9 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        for i in range(1, length):
-            nums[i] += nums[i - 1]
-        return nums
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+       for (int i = 1; i < nums.size(); i++){
+        nums[i] += nums[i - 1];
+    }
+    return nums; 
+    }
+};
