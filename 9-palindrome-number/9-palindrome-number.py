8ed93b84844558f6bed59b1a6f7986aece783917
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-05 20:45:33 +0800

    Time: 103 ms (32.22%), Space: 13.8 MB (96.59%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,18 +1,5 @@
-bool isPalindrome(int x){
-    if (x < 0){
-        return false;
-    }
-    
-    int temp = x;
-    
-    //Long to prevent overloading
-    long int save = 0;
-    
-    while (x != 0){
-        save = ((save * 10) + (x % 10));
-        x /= 10;
-    }
-    
-    return (save == temp);
-    
-}
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = f"{x}"
+        return (x == x[::-1])
+        
