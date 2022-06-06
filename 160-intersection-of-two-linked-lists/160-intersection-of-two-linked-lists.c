beba819d6d7c626bee4822d69521543eeb9a7b35
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-06 11:52:09 +0800

    Time: 66 ms (32.46%), Space: 14 MB (37.68%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.c
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.c
@@ -6,11 +6,17 @@
  * };
  */
 
-#include <string.h>
-
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    
+    // Steps to solving this problem
+    // Step 1: Create copy of both nodes and calculate the length of each node
+    // Logic: It is safe to assume that if the 2 nodes are of different length, any values before the index of the difference will not be the interesction point.
+    // Step 2: Based on which has the longer length, move the nodes length number of times to skip the redundant values to save on run time.
+    // Step 3: Compare the values of following node to find the proper intersection point, returning either of the 2 nodes as both points to the intersection point.
+    
     struct ListNode *nodeA = headA;
     struct ListNode *nodeB = headB;
+    
     int A = 0, B = 0;
     int diff = 0;
     
@@ -30,9 +36,11 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     else{
         diff = A - B;
     }
+    
     nodeA = headA;
     nodeB = headB;
     int count = 0;
+    
     while (count < diff){
         if (A < B){
             nodeB = nodeB->next;
@@ -42,6 +50,7 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
         }
         count++;
     }
+    
     while (nodeA != NULL){
         if (nodeA == nodeB){
             return nodeA;
