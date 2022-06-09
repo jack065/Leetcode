743e878abd1ef196ffc2632883797da77e8c9f65
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-09 10:52:45 +0800

    Time: 206 ms (34.32%), Space: 14.9 MB (41.52%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # How does the program work?
+        # Create 2 pointers at the start and end of the entire list
+        # Add up the values at the pointers
+        # If the sum of the values is smaller than the target, increment the lower pointer by one
+        # If the sum of the values is larger than the target, decrement the higher pointer by one
+        
+        # Another method is similar to the original two sum where you create a hashmap, compare the values
+        # then submit the indexes of the matching pairs.
+    
         start = 0
         end = len(numbers) - 1
-        
         while start != end:
             summer = numbers[start] + numbers[end]
             if summer > target:
@@ -10,5 +18,6 @@ class Solution:
             elif summer < target:
                 start += 1
             else:
+                # Return the pointers + 1
                 return [start + 1, end + 1]
             
