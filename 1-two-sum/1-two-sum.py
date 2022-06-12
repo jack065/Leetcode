a1189d23d54a1338ba22714b8fb037e5f26bea3f
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-12 17:26:14 +0800

    Time: 113 ms (45.52%), Space: 15.2 MB (48.93%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -8,6 +8,6 @@ class Solution:
         for i, n in enumerate(nums):
             diff = target - n
             if diff in hashMap:
-                return [hashMap[diff], i]
-            hashMap[n] = i 
+                return (i, hashMap[diff])
+            hashMap[n] = i
         return
