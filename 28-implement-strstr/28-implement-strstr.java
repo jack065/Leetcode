76830ecca8419b362c42e8ecbfacdd4ead80d6f7
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-21 10:45:34 +0800

    Time: 1 ms (62.74%), Space: 41.8 MB (60.17%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,9 +1,17 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        int s = haystack.length();
+        int l = needle.length();
+        
+        int i = 0, j = 0;
+        
         if (haystack.length() < needle.length()){
             return -1;
         }
-        int i = 0, j = 0;
+        if (haystack.equals(needle)){
+            return 0;
+        }
+        
         while (i < haystack.length()){
             if (haystack.charAt(i) == needle.charAt(j)){
                 int temp = i;
@@ -11,7 +19,7 @@ class Solution {
                     i++;
                     j++;
                     if (j == needle.length()){
-                    return temp;
+                        return temp;
                     }
                 }
                 i = temp;
