d844c43be90a4fa60c46de4b1773f8dbe2bd9f26
tomatoninja <90119304+jack065@users.noreply.github.com>
2022-06-21 10:53:31 +0800

    Time: 1 ms (62.74%), Space: 41.8 MB (60.17%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,20 +1,29 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        // Create variables to store the length of both strings
         int s = haystack.length();
         int l = needle.length();
         
+        // 2 pointer method
         int i = 0, j = 0;
         
+        // if the haystack is shorter than the needle, return -1 as no possible substring
         if (haystack.length() < needle.length()){
             return -1;
         }
+        
+        // if the haystack is the same as the needle, return 0 as they are the same thing
         if (haystack.equals(needle)){
             return 0;
         }
         
+        // 2 pointer method to iterate through both haystack and needle strings
+        // While loop activates for as long as i is smaller than length of haystack
         while (i < haystack.length()){
+            // If the character at the i index in haystack is the same as the character at the j index in needle, store i index as a temp variable.
             if (haystack.charAt(i) == needle.charAt(j)){
                 int temp = i;
+                // As long as i is smaller than the length of haystack, and the character at i is the same as the character at j, increase i and j till length of j is the same as length of needle. If not possible, reset i to temp and j back to zero
                 while (i < haystack.length() && haystack.charAt(i) == needle.charAt(j)){
                     i++;
                     j++;
@@ -25,6 +34,7 @@ class Solution {
                 i = temp;
                 j = 0;
             }
+            // Increment i by one with each loop.
             i++;
         }
         return -1;
